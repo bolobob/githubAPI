d979df78b63dfb54e2d19d1814395e991e8bfeed
bolobob <aokishinichi@gmail.com>
2013-05-22 22:35:28 +0900

    アバター表示できた

--- prototype/js/base.js
+++ prototype/js/base.js
@@ -1,5 +1,26 @@
 $(function() {
   'use strict';
 
-  
+  $.ajaxSetup({cache: false});
+
+  var App = {
+      base_url: 'https://api.github.com/users/',
+
+      init: function() {
+        $.ajax({
+          type: 'GET',
+          url: this.base_url + 'bolobob',
+          dataType: 'json',
+          success: function(data, textStatus, jqXHR) {
+            App.addAvatar(data.avatar_url);
+          }
+        });
+      },
+
+      addAvatar: function(url) {
+        $('#content').append($('<img>').attr('src', url));
+      }
+  };
+
+  App.init();
 });
