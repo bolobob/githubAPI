931d7e03a0f19d329da3c466584c6eb496f1b622
bolobob <aokishinichi@gmail.com>
2013-05-23 09:30:33 +0900

    svgで実装

--- prototype/js/base.js
+++ prototype/js/base.js
@@ -4,22 +4,29 @@ $(function() {
   $.ajaxSetup({cache: false});
 
   var App = {
-      base_url: 'https://api.github.com/users/',
+    base_url: 'https://api.github.com/users/',
 
-      init: function() {
-        $.ajax({
-          type: 'GET',
-          url: this.base_url + 'bolobob',
-          dataType: 'json',
-          success: function(data, textStatus, jqXHR) {
-            App.addAvatar(data.avatar_url);
-          }
-        });
-      },
+    init: function() {
+      this.cacheElements();
 
-      addAvatar: function(url) {
-        $('#content').append($('<img>').attr('src', url));
-      }
+      $.ajax({
+        type: 'GET',
+        url: this.base_url + 'bolobob',
+        dataType: 'json',
+        success: function(data, textStatus, jqXHR) {
+          App.renderUser(data);
+        }
+      });
+    },
+
+    cacheElements: function() {
+      this.canvas = SVG('canvas').size('100%', '100%');
+    },
+
+    renderUser: function(data) {
+      this.canvas.image(data.avatar_url).move(100, 100);
+      this.canvas.text(data.login).move(120, 200);
+    }
   };
 
   App.init();
