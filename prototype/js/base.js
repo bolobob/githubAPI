69fde07d4b543b442b605d93820b656c964fb7c5
bolobob <aokishinichi@gmail.com>
2013-05-28 00:34:23 +0900

    アバターを円でマスキングして影をつけた

--- prototype/js/base.js
+++ prototype/js/base.js
@@ -24,8 +24,20 @@ $(function() {
     },
 
     renderUser: function(data) {
-      this.canvas.image(data.avatar_url).move(100, 100);
+      // 円の影
+      var gradient = this.canvas.gradient('radial', function(stop) {
+                       stop.at({offset: 70, color: '#333', opacity: 1});
+                       stop.at({offset: 100, color: '#fff', opacity: 0});
+                     });
+      var circle_shadow = this.canvas.circle(80).attr({fill: gradient.fill()}).move(103, 103);
+
+      // アバター
+      var avatar = this.canvas.image(data.avatar_url).size(80, 80).move(100, 100);
       this.canvas.text(data.login).move(120, 200);
+
+      // マスキング
+      var ellipse = this.canvas.ellipse(60, 60).move(110, 110).fill({ color: '#fff' });
+      avatar.clipWith(ellipse);
     }
   };
 
