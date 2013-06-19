98284cd7f3b81e053f942a700613e8f067f4d856
bolobob <aokishinichi@gmail.com>
2013-06-19 23:59:00 +0900

    位置調整

--- prototype/js/base.js
+++ prototype/js/base.js
@@ -42,8 +42,8 @@ $(function() {
 
       // 円の影
       this.gradient = this.canvas.gradient('radial', function(stop) {
-                       stop.at({offset: 70, color: '#333', opacity: 1});
-                       stop.at({offset: 100, color: '#fff', opacity: 0});
+                        stop.at({offset: 70, color: '#333', opacity: 1});
+                        stop.at({offset: 100, color: '#fff', opacity: 0});
                      });
       this.circle_shadow = this.canvas.circle(80).attr({fill: this.gradient.fill()}).move(103, 103);
 
@@ -63,8 +63,8 @@ $(function() {
         dataType: 'json',
         success: function(data, textStatus, jqXHR) {
           $.each(data, function(idx) {
-            var y = idx * 100;
-            App.following.push(new App.Following(this, 250, y));
+            var y = idx * (App.Following.WIDTH+App.Following.MARGIN_BOTTOM);
+            App.following.push(new App.Following(this, 150, y));
           });
         }
       });
@@ -77,25 +77,33 @@ $(function() {
     var y = y;
 
     this.render = function() {
+      this.g = App.canvas.group().x(x);
+
       this.gradient = App.canvas.gradient('radial', function(stop) {
                        stop.at({offset: 70, color: '#333', opacity: 1});
                        stop.at({offset: 100, color: '#fff', opacity: 0});
                      });
-      this.circle_shadow = App.canvas.circle(80).attr({fill: this.gradient.fill()}).move(x+3, y+3);
+      this.circle_shadow = App.canvas.circle(App.Following.WIDTH).attr({fill: this.gradient.fill()}).move(x+3, y+3);
 
       // アバター
-      this.avatar = App.canvas.image(data.avatar_url).size(80, 80).move(x, y);
+      this.avatar = App.canvas.image(data.avatar_url).size(App.Following.WIDTH, App.Following.HEIGHT).move(x, y);
 
       // 名前
-      this.name = App.canvas.text(data.login).move(x+20, y+80);
+      this.name = App.canvas.text(data.login).cx(x+(App.Following.WIDTH/2)).y(y+App.Following.HEIGHT);
 
       // マスキング
       this.ellipse = App.canvas.ellipse(60, 60).move(x+10, y+10).fill({ color: '#fff' });
       this.avatar.clipWith(this.ellipse);
+
+      this.g.add(this.circle_shadow);
+      this.g.add(this.avatar);
+      this.g.add(this.name);
     };
 
     this.render();
   };
-
+  App.Following.WIDTH  = 80;
+  App.Following.HEIGHT = 80;
+  App.Following.MARGIN_BOTTOM = 40;
   App.init();
 });
